5f98f6620616fa89789648f4182757269f39e89a
EnesErdogan <enes.erdgn32@gmail.com>
2023-09-20 20:23:09 +0300

    first commit

---
+++ src/main/java/utils/elementHelper.java
@@ -0,0 +1,21 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+
+public class elementHelper {
+
+    AppiumDriver driver;
+
+    public elementHelper(AppiumDriver appiumDriver) {
+        driver = appiumDriver;
+    }
+
+    public void findElement() {
+        driver.findElement(By.xpath(""));
+    }
+
+
+
+
+}
